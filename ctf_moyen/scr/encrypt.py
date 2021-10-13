ef67bf9161c07a0fdd9be8c3c58e3fa153a12e7e
enzo.hmdy <enzo.hmdy@gmail.com>
2021-10-13 10:30:08 -0400

    ctf_moyen/src : encrypt just need to change binairie of 0

--- ctf_moyen/scr/encrypt.py
+++ ctf_moyen/scr/encrypt.py
@@ -6,6 +6,7 @@ usage python3 encrypt input_file output_file message
 
 """
 
+from os import putenv
 from PIL import Image
 import numpy as np
 import sys
@@ -20,23 +21,45 @@ def convert_msg_to_binary(msg):
 def stegano(input_file, output_file, msg):
 
     img = Image.open(input_file)
+    bin_msg = convert_msg_to_binary(msg)
+    print(bin_msg)
     bin_msg_lenght = len(convert_msg_to_binary(msg))
     width, height = img.size
     img_data = np.array(img)
     img_data = np.reshape(img_data, width*height*3)
+    print(img_data)
     # data[:b_message_lenght] = data[:b_message_lenght] & ~1 | b_message
-    img_data[:bin_msg_lenght] = img_data[:bin_msg_lenght] & ~1 | convert_msg_to_binary(
+
+    for i in range(len(bin_msg)):
+        print("Avant : ", img_data[i])
+        print("bin :", bin_msg[i])
+
+        if(bin_msg[i] and not img_data[i] % 2):
+            print("eee")
+            img_data[i] = img_data[i] - 1
+
+        else:
+
+            print(bin_msg[i] == bin(0), "et", img_data[i] % 2)
+            print(bin(0))
+
+        if((bin_msg[i] == bin(0)) and (img_data[i] % 2)):
+            print("poouet")
+            img_data[i] = img_data[i] + 1
+
+        print("Après : ", img_data[i])
+
+    """img_data[:bin_msg_lenght] = img_data[:bin_msg_lenght] & ~1 | convert_msg_to_binary(
         msg)
     img_data = np.reshape(img_data, (height, width, 3))
 
     new_img = Image.fromarray(img_data)
-    new_img.save(output_file)
+    new_img.save(output_file)"""
 
 
 def main(argv):
     msh_lenght = len(argv) - 3
-    stegano(argv[1], argv[2], "message")
-    print(msh_lenght)
+    stegano(argv[1], argv[2], "mellier ce gros bg de la street")
 
 
 main(sys.argv)
