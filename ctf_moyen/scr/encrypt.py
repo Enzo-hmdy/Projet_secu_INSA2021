2e247a85db6beb67b33c47443a0224e5fdb19abe
enzo.hmdy <enzo.hmdy@gmail.com>
2021-10-13 08:23:12 -0400

    ctf_moyen/src : convert msg to binary done include an image for test

--- ctf_moyen/scr/encrypt.py
+++ ctf_moyen/scr/encrypt.py
@@ -5,3 +5,25 @@ usage python3 encrypt input_file output_file message
 
 
 """
+
+from PIL import Image
+import numpy as np
+
+
+def convert_msg_to_binary(msg):
+
+    return ''.join(["{:08b}".format(ord(x)) for x in msg])
+
+
+def stegano(input_file, output_file, msg):
+
+    img = Image.open(input_file)
+
+    weight, height = img.size
+
+
+def main():
+    print(convert_msg_to_binary("*£"))
+
+
+main()
