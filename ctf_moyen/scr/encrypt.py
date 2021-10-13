745fe30aa4053460fea3be3c3ef026574aef8e0a
enzo.hmdy <enzo.hmdy@gmail.com>
2021-10-13 09:19:41 -0400

    ctf_moyen/src : encrypt.py almost done

--- ctf_moyen/scr/encrypt.py
+++ ctf_moyen/scr/encrypt.py
@@ -8,6 +8,8 @@ usage python3 encrypt input_file output_file message
 
 from PIL import Image
 import numpy as np
+import sys
+import getopt
 
 
 def convert_msg_to_binary(msg):
@@ -18,12 +20,23 @@ def convert_msg_to_binary(msg):
 def stegano(input_file, output_file, msg):
 
     img = Image.open(input_file)
+    bin_msg_lenght = len(convert_msg_to_binary(msg))
+    width, height = img.size
+    img_data = np.array(img)
+    img_data = np.reshape(img_data, width*height*3)
+    # data[:b_message_lenght] = data[:b_message_lenght] & ~1 | b_message
+    img_data[:bin_msg_lenght] = img_data[:bin_msg_lenght] & ~1 | convert_msg_to_binary(
+        msg)
+    img_data = np.reshape(img_data, (height, width, 3))
 
-    weight, height = img.size
+    new_img = Image.fromarray(img_data)
+    new_img.save(output_file)
 
 
-def main():
-    print(convert_msg_to_binary("*£"))
+def main(argv):
+    msh_lenght = len(argv) - 3
+    stegano(argv[1], argv[2], "message")
+    print(msh_lenght)
 
 
-main()
+main(sys.argv)
