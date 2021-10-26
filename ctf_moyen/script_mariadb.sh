96ab3a561ce1241f7ee4558f11c2d10a5d829885
enzo-hmdy <enzo.hmdy@gmail.com>
2021-10-26 23:55:00 +0200

    IT WORKS CTF MOYEN ALE

--- ctf_moyen/script_mariadb.sh
+++ ctf_moyen/script_mariadb.sh
@@ -4,7 +4,7 @@ sudo apt -y install mariadb-server mariadb-client
 MYSQL_ROOT_PASSWORD=azerty
 MYSQL_INSTAL=$(expect -c "
 
-set timeout 10
+set timeout 5
 spawn mysql_secure_installation
 expect \"Enter current password for root:\"
 send \"$MYSQL_ROOT_PASSWORD\r\"
@@ -15,7 +15,7 @@ send \"n\r\"
 expect \"Remove anonymous users?\"
 send \"y\r\"
 expect \"Disallow root login remotely?\"
-send \"y\r\"
+send \"n\r\"
 expect \"Remove test database and access to it?\"
 send \"y\r\"
 expect \"Reload privilege tables now?\"
@@ -24,3 +24,10 @@ expect eof
 ")
 
 echo "$MYSQL_INSTAL"
+ufw allow from 0.0.0.0 to any port 3306
+iptables -A INPUT -p tcp --dport 3306 -j ACCEPT
+service iptables save
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root';"
+sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf 
+systemctl restart mysql
+systemctl restart mariadb
