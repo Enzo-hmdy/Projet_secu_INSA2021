f88a8e3337d9285ba3935deabb4296f816723af6
enzo-hmdy <enzo.hmdy@gmail.com>
2021-11-04 09:08:58 +0100

    ctf_moyen : Install fail2ban and his conf + walktrough

---
+++ ctf_moyen/documentation/walktrough.md
@@ -0,0 +1,14 @@
+# Walktrought CTF_Moyen_2020-2021 Groupe 4
+
+## Avant Propos :
+
+Ce CTF s'axe sur 3 catégories : 
+- De la steganographie
+- De la cryptographie
+- Du système
+
+
+
+## Etape 1
+
+L'utilisateur lorsque qu'il est sur la machine n'aura en premier lieu accès à un /home où il n'y aura de présent que plusieurs images, dont une contiendra 
--- ctf_moyen/script_mariadb.sh
+++ ctf_moyen/script_mariadb.sh
@@ -31,3 +31,13 @@ mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'
 sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf 
 systemctl restart mysql
 systemctl restart mariadb
+MYMSG=$(cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 6; echo;)
+sudo apt install fail2ban -y
+touch /etc/fail2ban/jail.d/mariadb.conf
+echo '[mysqld-auth]
+enabled = true
+filter   = mysqld-auth
+port     = 3306
+maxretry = 3
+bantime = 600
+logpath  = /var/log/mariadb/mariadb.log' >> /etc/fail2ban/jail.d/mariadb.conf
