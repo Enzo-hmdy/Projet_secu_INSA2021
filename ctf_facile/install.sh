7de2f9836eb0d428d97bb7f4fb2d2e9ada289258
Dorian Beaufils <dorian.beaufils@gmail.com>
2021-11-08 09:35:34 -0500

    script final

--- ctf_facile/install.sh
+++ ctf_facile/install.sh
@@ -2,25 +2,16 @@
 iptables -F 
 iptables -X
 
-apt update
-
-GITUSERNAME=Nyzix69
-GITPASSWORD=y4Du%&mjZz#G
 # INSTALLATION DES PAQUETS NECESSAIRES
-apt install -y expect
-apt install -y apache2 php
-apt install -y postgresql postgresql-client
-apt install -y git
-
-
+apt update
+#apt install -y expect
+apt install -y apache2 php postgresql postgresql-client git
 
 #IMPORTATION DU GIT AVEC LES FICHIERS .PHP ET .HTML
 cd /home/debian/ 
-
 git clone https://github.com/projetsecu/projetsecurite.git
-
-
-
+cp /home/debian/projetsecurite/ctf_facile/web/* /var/www/html/ 
+rm /var/www/html/index.html
 
 
 #DEMARRAGE DU SERVEUR WEB APACHE 2
@@ -28,17 +19,23 @@ systemctl start apache2
 
 #DEMARRAGE DE POSTGRESQL
 systemctl enable --now postgresql
-#su - postgres
-psql -c "ALTER USER postgres WITH password 'azerty'" #A CHANGER
-createdb ctf_facile -O postgresql
-psql -U postgres -d ctf_facile -c "CREATE TABLE users (id varchar(25) PRIMARY KEY, password varchar(50);"
-psql -U postgres -d ctf_facile -c "COPY users FROM 'CHEMIN/VERS/FICHIER.CSV' WITH (FORMAT CSV, HEADER, DELIMITER';',QUOTE "");" #TODO
+#set timeout 2
+useradd admin -p azerty
 
-cp /home/debian/projetsecu/ctf_facile/web/* /var/www/html/ 
+MYMSG=$(cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 6; echo;)
+
+sudo -u postgres psql  -c "ALTER USER postgres WITH password '$MYMSG'" 
+sudo -u postgres createuser -d admin
+#sudo -u postgres psql  -c "ALTER USER admin WITH password 'azerty'" 
+sudo -u postgres createdb ctf_facile -O admin
+sudo -u postgres psql -d ctf_facile -c "CREATE TABLE users (id varchar(25) PRIMARY KEY, password varchar(50));"
+sudo -u postgres psql -U postgres -d ctf_facile -c "COPY users FROM '/home/debian/projetsecurite/ctf_facile/bdd_users.csv' WITH (FORMAT CSV, HEADER, DELIMITER ',', QUOTE '''');" 
+#sudo -u postgres psql -c "alter database ctf_facile owner to admin;"
+#sudo -u postgres psql -d ctf_facile -c "alter table users owner to admin;"
+
+systemctl restart postgresql
 
 #OUVERTURE D'UN PORT SSH ET D'UN PORT HTTP UNIQUEMENT
-
-
 iptables -t filter -A INPUT -i lo -j ACCEPT
 
 iptables -t filter -A INPUT -p tcp --dport 22 -j ACCEPT 
@@ -51,4 +48,5 @@ iptables -t filter -A OUTPUT -p tcp --sport 80 -j ACCEPT
 iptables -t filter -A INPUT -j DROP
 iptables -t filter -A OUTPUT -j DROP
 
+rm -r /home/debian/projetsecurite/
 #rm -rf /home/debian/Projet_secu_INSA2021/
