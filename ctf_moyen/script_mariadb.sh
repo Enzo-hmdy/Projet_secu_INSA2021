881a3c8aa4c617770ccfbef47b1cdc43141f8965
PlacardDuFutur <clement.mellier2103@gmail.com>
2021-11-22 22:07:27 +0100

    event sql

--- ctf_moyen/script_mariadb.sh
+++ ctf_moyen/script_mariadb.sh
@@ -66,3 +66,60 @@ echo "-----------IPTABLES CREATE RULE----------" >> /tmp/install.log
 iptables -I INPUT -p tcp --dport 3306 -i eth0 -m state --state NEW -m recent  --update --seconds 300 --hitcount 4 -j DROP  >> /tmp/install.log
 echo "-----------IPTABLES SET RULE-----------" >> /tmp/install.log
 
+
+CREATE DATABASE IF NOT EXISTS script; # Create database
+SHOW DATABASES; # show all databases
+use script; # select a database
+
+CREATE TABLE test ( id INT PRIMARY KEY, nom VARCHAR(100)); # Create a table 
+
+INSERT INTO test (id,nom) VALUES (1,"ouioui");
+INSERT INTO test (id,nom) VALUES (2,"nonnon");
+INSERT INTO test (id,nom) VALUES (3,"peutetre");
+
+
+DELIMITER | 
+CREATE PROCEDURE afficher_test()      
+BEGIN
+    SELECT id, nom
+    FROM test;
+END| 
+
+SET GLOBAL event_scheduler=ON;
+
+DBMS_SCHEDULER.create_program
+(
+program_name => 'sch_program',
+program_type => 'EXECUTABLE',
+program_action => '/home/debiane/shell.sh',
+number_of_arguments => 0,
+enabled => TRUE,
+comments => 'Test Program'
+);
+end;
+/
+
+DROP EVENT e_daily;
+
+delimiter |
+
+CREATE EVENT e_daily
+    ON SCHEDULE
+      EVERY 20 SECOND
+    COMMENT 'Saves total number of sessions then clears the table each day'
+    DO
+      BEGIN
+        program_action => '/home/debian/shell.sh',
+      END |
+
+delimiter ;
+
+DBMS_SCHEDULER.CREATE_JOB (
+    job_name          => 'TEST_SHELL',
+    job_type          => 'EXECUTABLE',
+    job_action        => '/home/debian/shell.sh',
+    start_date        => SYSDATE,
+    --repeat_interval   => 'FREQ=MINUTELY; INTERVAL=1',  
+    enabled           => TRUE,
+    comments          => 'Calling shell script from Oracle' 
+    );
