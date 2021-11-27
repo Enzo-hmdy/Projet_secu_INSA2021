b99d03da427e015f0a8c9406b75fe2554b1f76b3
PlacardDuFutur <clement.mellier2103@gmail.com>
2021-11-27 15:29:19 +0100

    script capslock del

--- ctf_moyen/scr/create_db.sql
+++ ctf_moyen/scr/create_db.sql
@@ -1,12 +1,12 @@
 USE scripts
 
-CREATE TABLE SCRIPT (
+CREATE TABLE script (
     nom_script VARCHAR(50) PRIMARY KEY,
     path_script VARCHAR(200),
     text_script TEXT
     );
 
-INSERT INTO SCRIPT (nom_script,path_script,text_script)
+INSERT INTO script (nom_script,path_script,text_script)
 VALUES
 ('show_in_console.sh','/home/debian/protected_script','#!/bin/bash \n \n'),
 ('clock.sh','/home/debian/protected_script','#!/bin/bash date +%R >> "/home/debian/clock.txt"'),
