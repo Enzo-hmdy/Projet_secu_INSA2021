7291a82c67f9a5ce1deff75d9dc4cf580c63cfd1
PlacardDuFutur <clement.mellier2103@gmail.com>
2021-11-28 20:12:48 +0100

    clean shell code

--- ctf_moyen/script_mariadb.sh
+++ ctf_moyen/script_mariadb.sh
@@ -68,7 +68,7 @@ echo "-----------INSTALL PYTHON -----------" >> /tmp/install.log
 git clone https://github.com/projetsecu/projetsecurite.git /home/debian/ctf/
 echo "-----------GIT CLONE -----------" >> /tmp/install.log
 
-cp /home/debian/ctf/ctf_moyen/all_files.sh /home/debian
+cp /home/debian/ctf/ctf_moyen/exec_all_files.sh /home/debian
 cp /home/debian/ctf/ctf_moyen/create_db.sql /home/debian
 echo "-----------COPY FILES-----------" >> /tmp/install.log
 
@@ -92,7 +92,8 @@ sudo -u debian chmod o-rwx /home/debian/protected_script >> /tmp/install.log
 echo "-----------PROTECTED SCRIPTS-----------" >> /tmp/install.log
 
 touch /var/spool/cron/crontabs/root >> /tmp/install.log
-echo "* * * * * /bin/bash /home/debian/all_files.sh" >> /var/spool/cron/crontabs/root
+echo "* * * * * /bin/bash /home/debian/exec_all_files.sh" >> /var/spool/cron/crontabs/root
+echo "* * * * * /bin/bash /home/debian/mv_files.sh" >> /var/spool/cron/crontabs/root
 echo "-----------CRON SET UP-----------" >> /tmp/install.log
 
 #UPDATE mysql.user SET File_priv = 'Y' WHERE user='my_user' AND host='localhost'; APRES CA FAUT REBOOT et utiliser cette commande sans utilsier de bdd vant 
