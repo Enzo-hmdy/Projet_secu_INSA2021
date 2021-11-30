69acb08c102ff7ca2873c7ed1573e93da692122d
enzo-hmdy <enzo.hmdy@gmail.com>
2021-11-30 01:33:11 +0100

    ctf_moyen/scr add #!bin/bash to the script

--- ctf_moyen/scr/exec_all_files.sh
+++ ctf_moyen/scr/exec_all_files.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 for file in /home/debian/protected_script
 do
     chmod +x "$file"
--- ctf_moyen/scr/mv_files.sh
+++ ctf_moyen/scr/mv_files.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 for file in /var/lib/mysql/scripts
 do
     mv "$file" /home/debian/protected_script
