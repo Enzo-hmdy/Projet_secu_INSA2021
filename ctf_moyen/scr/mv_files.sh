055763bbf27065094d8bd232471ad11a6567565e
PlacardDuFutur <clement.mellier2103@gmail.com>
2021-11-30 16:29:32 +0100

    [CTF_moyen] fix mov_files

--- ctf_moyen/scr/mv_files.sh
+++ ctf_moyen/scr/mv_files.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-for file in /var/lib/mysql/scripts
+for file in /var/lib/mysql/scripts/*
 do
+    if [ -f $file -a -x $file ]
+then
     mv "$file" /home/debian/protected_script
+fi
+    
 done
