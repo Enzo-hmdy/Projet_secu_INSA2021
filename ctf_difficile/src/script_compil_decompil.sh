2851c1e4f195183614a1320e51a3200e08d82d57
nath014 <nathan.rochas014@gmail.com>
2022-01-11 11:41:37 +0100

    modif pyinstaller

--- ctf_difficile/src/script_compil_decompil.sh
+++ ctf_difficile/src/script_compil_decompil.sh
@@ -5,13 +5,14 @@ apt install python3-pip
 python3 -m pip install --upgrade pip
 python3 -m pip install pyinstaller==4.6
 python3 -m pip install numpy
+python3 -m pip install pycryptodomex
 cd /home/nrochas/Documents/projet_secu
-python3 -m PyInstaller --onefile test.py
+python3 -m PyInstaller --onefile encrypt.py
 apt install git
 cd ..
 git clone https://github.com/extremecoders-re/pyinstxtractor.git
 cd projet_secu
-python3 ../pyinstxtractor/pyinstxtractor.py dist/test
+python3 ../pyinstxtractor/pyinstxtractor.py dist/encrypt
 python3 -m pip install uncompyle6
-uncompyle6 test_extracted/test.pyc > test_src.py
-cat test_src.py
+uncompyle6 encrypt_extracted/encrypt.pyc > src_encrypt.py
+cat src_encrypt.py
